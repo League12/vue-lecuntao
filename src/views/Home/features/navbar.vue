bca64d25e8b31345ece3bdcf35d39d280e85e4d2
annan10005 <508780180@qq.com>
2019-07-12 13:13:53 +0800

    合并前

--- src/views/Home/features/navbar.vue
+++ src/views/Home/features/navbar.vue
@@ -10,6 +10,7 @@
 <script>
 export default {}
 </script>
+
 <style lang="scss" scoped>
 div{
     ul{
@@ -30,3 +31,14 @@ div{
   border-bottom: 2px solid red;
 }
 </style>
+
+class Animal{
+    constructor(){//这个constructor方法内定义的方法和属性是实例化对象自己的，不共享；construstor外定义的方法和属性是所有实例对象（共享）可以调用的
+         this.type = 'animal'  //this关键字代表Animal对象的实例对象 
+     }
+     says(say){
+         console.log(this.type+' says ' +say); 
+     }
+}
+let animal = new Animal();
+animal.says('hello')
